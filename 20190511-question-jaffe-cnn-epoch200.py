0f4e4511b7853ce385f2f40a6b0c3146c31a59a9
12 22 <s_laihuiqian@kingsoft.com>
2019-05-11 20:18:52 +0800

    20190511 add epoch =20 to improve the acc in jaffe cnn

---
+++ 20190511-question-jaffe-cnn-epoch200.py
@@ -0,0 +1,365 @@
+
+# coding: utf-8
+
+# In[51]:
+
+
+import os
+import cv2
+import numpy as np
+from PIL import Image
+from keras.applications.vgg16 import VGG16
+from keras.utils import to_categorical
+import keras
+from keras import layers
+from keras.layers import *
+from keras.models import Model, Sequential
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+import h5py
+from matplotlib import pyplot as plt
+from PIL import Image
+from scipy.io import loadmat
+
+
+# In[52]:
+
+
+data = loadmat('jaffe_mean_data.mat')['jaffe_mean_data']
+
+
+# In[53]:
+
+
+data = data.reshape((181,256,256,1))
+
+
+# In[54]:
+
+
+label = loadmat('cl_label.mat')['cl_label']
+
+
+# In[55]:
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(data, label, test_size=0.2)
+
+
+# In[56]:
+
+
+print('Training: ',X_train.shape)
+
+
+# In[57]:
+
+
+from keras.layers import Convolution2D, Activation, BatchNormalization, MaxPooling2D, Dropout, Dense, Flatten, AveragePooling2D
+
+
+# In[58]:
+
+
+from keras.models import Sequential
+
+
+# In[59]:
+
+
+def get_nn_model():
+    model = Sequential()
+    model.add(MaxPooling2D(pool_size = (4,4), strides=None,input_shape=(256,256,1)))
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+#     model.add(Dropout(0.5))
+
+    model.add(Dense(128))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dense(128))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(7))
+    model.add(Activation('softmax'))
+    return model
+
+
+# In[60]:
+
+
+def get_cnn_model():
+    model = Sequential()
+    model.add(Convolution2D(64, (3, 3), padding='same',input_shape=(256,256,1)))
+    model.add(Convolution2D(64, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(128, (3, 3), padding='same'))
+    model.add(Convolution2D(128, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(256, (3, 3), padding='same'))
+    model.add(Convolution2D(256, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(512, (3, 3), padding='same'))
+    model.add(Convolution2D(512, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+
+    model.add(Dense(1024))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.25))
+
+    model.add(Dense(1024))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(7))
+    model.add(Activation('softmax'))
+    return model
+
+
+# In[61]:
+
+
+cnn_model = get_cnn_model()
+
+
+# In[62]:
+
+
+nn_model = get_nn_model()
+
+
+# In[63]:
+
+
+nn_model.summary()
+
+
+# In[64]:
+
+
+cnn_model.summary()
+
+
+# In[65]:
+
+
+from keras.preprocessing.image import ImageDataGenerator 
+
+
+# In[66]:
+
+
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+
+
+# In[67]:
+
+
+from keras import backend as K
+
+
+# In[68]:
+
+
+def fbeta(y_true, y_pred, threshold_shift=0):
+    beta = 1
+
+    # just in case of hipster activation at the final layer
+    y_pred = K.clip(y_pred, 0, 1)
+
+    # shifting the prediction threshold from .5 if needed
+    y_pred_bin = K.round(y_pred + threshold_shift)
+
+    tp = K.sum(K.round(y_true * y_pred_bin), axis=1) + K.epsilon()
+    fp = K.sum(K.round(K.clip(y_pred_bin - y_true, 0, 1)), axis=1)
+    fn = K.sum(K.round(K.clip(y_true - y_pred, 0, 1)), axis=1)
+
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+
+    beta_squared = beta ** 2
+    return K.mean((beta_squared + 1) * (precision * recall) / (beta_squared * precision + recall + K.epsilon()))
+
+
+# In[69]:
+
+
+def train_cnn_model(model):
+    filepath='../opt/jaffe-cnn/Model.{epoch:02d}-{val_acc:.4f}.hdf5'
+    checkpointer = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=False, mode='auto')
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=0, mode='auto', cooldown=0, min_lr=0)
+    early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='auto')
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.0,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.0,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False,   # randomly flip images
+        )
+
+
+    datagen.fit(X_train)
+    datagen.fit(X_test)    
+    batch_size = 16
+
+    num_epochs = 200
+    model.compile(loss='categorical_crossentropy',
+             optimizer='adam',
+             metrics=[fbeta, 'acc'])
+    train_flow = datagen.flow(X_train, Y_train, batch_size=batch_size)
+    validation_flow = datagen.flow(X_test, Y_test)
+    history = model.fit_generator(train_flow,
+                    steps_per_epoch=len(X_train) / batch_size,
+                    epochs=num_epochs, 
+                    verbose=1, 
+                    validation_data=validation_flow, 
+                    validation_steps=len(X_test) / batch_size,
+                    callbacks=[checkpointer, reduce_lr, checkpointer])
+    return history, model
+
+
+# In[70]:
+
+
+def train_nn_model(model):
+    filepath='../opt/jaffe-nn/Model.{epoch:02d}-{val_acc:.4f}.hdf5'
+    checkpointer = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=False, mode='auto')
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=0, mode='auto', cooldown=0, min_lr=0)
+    early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='auto')
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.0,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.0,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False,   # randomly flip images
+        )
+
+
+    datagen.fit(X_train)
+    datagen.fit(X_test)    
+    batch_size = 32
+
+    num_epochs = 200
+    model.compile(loss='categorical_crossentropy',
+             optimizer='sgd',
+             metrics=[fbeta, 'acc'])
+    train_flow = datagen.flow(X_train, Y_train, batch_size=batch_size)
+    validation_flow = datagen.flow(X_test, Y_test)
+    history = model.fit_generator(train_flow,
+                    steps_per_epoch=len(X_train) / batch_size,
+                    epochs=num_epochs, 
+                    verbose=1, 
+                    validation_data=validation_flow, 
+                    validation_steps=len(X_test) / batch_size,
+                    callbacks=[checkpointer, reduce_lr, checkpointer])
+    return history, model
+
+
+# In[71]:
+
+
+cnn_history, cnn_model = train_cnn_model(cnn_model)
+
+
+# In[72]:
+
+
+nn_history, nn_model = train_nn_model(nn_model)
+
+
+# In[73]:
+
+
+n_classes = label.shape[1]
+
+
+# In[74]:
+
+
+labels=range(n_classes)
+
+
+# In[75]:
+
+
+def eval_model(history, model):
+    batch_size = 32
+    score = model.evaluate(X_test, Y_test, steps=(int)(len(X_test) / batch_size))
+    print('Evaluation loss: ', score[0])
+    print('Evaluation accuracy: ', score[1])
+    # summarize history for accuracy
+    plt.plot(history.history['acc'], color='b', label='Training')
+    plt.plot(history.history['val_acc'], color='g', label='Validation')
+    plt.title('Accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(loc='upper left')
+    plt.show()
+
+    # summarize history for loss
+    plt.plot(history.history['loss'], color='b', label='Training')
+    plt.plot(history.history['val_loss'], color='g', label='Validation')
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(loc='lower left')
+    plt.show()
+    y_pred = model.predict_classes(X_test)
+    y_true = np.asarray([np.argmax(i) for i in Y_test])
+
+    from sklearn.metrics import confusion_matrix
+    import seaborn as sns
+
+    cm = confusion_matrix(y_true, y_pred)
+    cm_normalised = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    sns.set(font_scale=1.5) 
+    fig, ax = plt.subplots(figsize=(10,10))
+    ax = sns.heatmap(cm_normalised, annot=True, linewidths=0, square=False, 
+                        cmap="Greens", yticklabels=labels, xticklabels=labels, vmin=0, vmax=np.max(cm_normalised), 
+                        fmt=".2f", annot_kws={"size": 20})
+    ax.set(xlabel='Predicted label', ylabel='True label')
+
+
+# In[76]:
+
+
+eval_model(cnn_history, cnn_model)
+
+
+# In[77]:
+
+
+eval_model(nn_history, nn_model)
+
