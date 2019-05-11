f2968467715350b21eb50677e8bd49cdbf9a8512
12 22 <s_laihuiqian@kingsoft.com>
2019-05-11 19:30:29 +0800

    20190511 reducing the dimension of PCA

--- 20190511-ck-svm.py
+++ 20190511-ck-svm.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[23]:
+# In[1]:
 
 
 import os
@@ -19,109 +19,111 @@ from time import time
 import logging#程序进展信息
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split#分割数据集
-#from sklearn.cross_validation import train_test_split
-from sklearn.datasets import fetch_lfw_people#下载数据集
 from sklearn.model_selection import GridSearchCV
-#from sklearn.grid_search import GridSearchCV
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
-#from sklearn.decomposition import RandomizedPCA
+from sklearn.decomposition import RandomizedPCA
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 
 
-# In[24]:
+# In[2]:
 
 
 ck_data = h5py.File('./CK_data.h5', 'r', driver='core')
-
-
-# In[25]:
-
-
 X_data = np.expand_dims(np.asarray(ck_data['data_pixel']), axis=-1)
-
-
-# In[26]:
-
-
 X_data = X_data.reshape((981,48*48))
+y_data = np.asarray(ck_data['data_label'])
+y_data = to_categorical(y_data)
+n_classes = y_data.shape[1] #列维数
 
 
-# In[27]:
+# In[3]:
 
 
-Y_data = np.asarray(ck_data['data_label'])
+print(ck_data)
 
 
-# In[28]:
+# In[4]:
 
 
-Y_data = to_categorical(Y_data)
+print(X_data)
 
 
-# In[29]:
+# In[5]:
 
 
-n_classes = Y_data.shape[1]
+print(y_data)
 
 
-# In[30]:
+# In[6]:
 
 
-X_train, X_test, Y_train, Y_test = train_test_split(X_data, Y_data, test_size=0.2)
+print(n_classes)
 
 
-# In[31]:
+# In[7]:
+
+
+print(y_data.shape[0])
+
+
+# In[8]:
+
+
+X_train, X_validation, y_train, y_validation = train_test_split(X_data, y_data, test_size=0.2)
+
+
+# In[9]:
 
 
 print('Training: ',X_train.shape)
 
 
-# In[32]:
+# In[10]:
 
 
-print('Validation: ',X_test.shape)
+print('Validation: ',X_validation.shape)
 
 
-# In[33]:
+# In[11]:
 
 
-#n_components = 64
+#使用主成分分析(PCA)降维
 n_components = 10
+#因为特征值维度比较高，所以需要降低维度，提取特征
+pca =PCA(svd_solver='randomized',n_components=n_components,whiten=True).fit(X_train)
+#得到训练集投影系数
+X_train_pca = pca.transform(X_train)
+#得到测试集投影系数
+X_val_pca = pca.transform(X_validation) 
 
 
-# In[34]:
+# In[12]:
 
 
-pca =PCA(svd_solver='randomized',n_components=n_components,whiten=True).fit(X_train)#降低维度
+print(X_train_pca)
 
 
-# In[35]:
+# In[13]:
 
 
-X_train_pca = pca.transform(X_train)      #得到训练集投影系数
+print(X_val_pca)
 
 
-# In[36]:
+# In[14]:
 
 
-X_val_pca = pca.transform(X_test) 
+print(X_train)
 
 
-# In[37]:
+# In[15]:
 
 
 eigenfaces = pca.components_.reshape((n_components, 48, 48))
 
 
-# In[38]:
-
-
-eigenfaces.shape
-
-
-# In[39]:
+# In[19]:
 
 
 def plot_gallery(images, titles, h, w, n_row=3, n_col=3):
@@ -134,27 +136,42 @@ def plot_gallery(images, titles, h, w, n_row=3, n_col=3):
         plt.title(titles[i], size=12)
         plt.xticks(())
         plt.yticks(())
- 
+
+
+# In[20]:
+
+
 eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
+
+
+# In[21]:
+
+
 plot_gallery(eigenfaces, eigenface_titles, 48, 48)
+
+
+# In[22]:
+
+
 plt.show()
 
 
-# In[40]:
+# In[23]:
 
 
-y_train_ = np.argmax(Y_train, axis=-1)
+y_train_ = np.argmax(y_train, axis=-1)
 
 
-# In[41]:
+# In[24]:
 
 
 y_train_.shape
 
 
-# In[42]:
+# In[25]:
 
 
+#训练SVM分类器
 print("Fitting the classifier to the training set")
 t0 = time()
 param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
@@ -168,14 +185,15 @@ print("Best estimator found by grid search:")
 print(clf.best_estimator_)
 
 
-# In[43]:
+# In[26]:
 
 
+#验证
 print("Predicting people's names on the test set")
 t0 = time()
 y_pred = clf.predict(X_val_pca)
 print("done in %0.3fs" % (time() - t0))
-y_val = np.argmax(Y_test, axis=-1)
+y_val = np.argmax(y_validation, axis=-1)
 print(classification_report(y_val, y_pred))
 print(confusion_matrix(y_val, y_pred, labels=range(n_classes)))
 
